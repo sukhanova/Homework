30c15b1c662555b07db91a33b2d350b27678cfad
Vira Sukhanova <verusia.s@gmail.com>
2021-04-03 08:59:14 -0500

    added BankAccount class and method that creates bank account with beginning balance set to 0

---
+++ bank-account-class/bank_acount.py
@@ -0,0 +1,36 @@
+# FIXME: Add Bank Account class definition here
+class BankAccount:
+    """A bank account"""
+    def __init__(self, beginning_balance = 0):
+        self.account_balance = beginning_balance
+        
+# You can see how this class should be used below. Make sure you
+# run your code and test it out to make sure it works.
+
+account_1 = BankAccount()
+account_2 = BankAccount()
+account_3 = BankAccount(100)
+print(f"account_1 balance is {account_1.account_balance}")
+print(f"account_2 balance is {account_2.account_balance}")
+print(f"account_3 balance is {account_3.account_balance}")
+# account_3.balance
+#   >>> 100
+# account_1.deposit(100)
+# account_1.balancecode 
+#   >>> 100
+# account_2.deposit(50)
+# account_2.balance
+#   >>> 50
+# account_3.deposit(75)
+# account_3.balance
+#   >>> 175
+# account_2.withdraw(10)
+# account_2.balance
+#   >>> 40
+# account_1.withdraw(10)
+# account_1.balance
+#   >>> 90
+# account_2.withdraw(50)
+#   >>> "Invalid transaction: not enough funds to withdraw $50." 
+# account_2.balance
+#   >>> 40    # still 40 since the previous transaction was not successful
