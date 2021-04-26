765d091cbd43c736456c89cac6ede17bedf2e1af
Vira Sukhanova <verusia.s@gmail.com>
2021-04-25 21:25:33 -0500

    Converted count_melons function from python to javascript

---
+++ javascript/melonCounting.js
@@ -0,0 +1,29 @@
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+
+function count_melons(melonList){
+    // Take an array and return an object
+    const melonCounts = {}
+
+    for(const melon of melonList){
+        // console.log(melon)
+    if (melon in melonCounts){
+        melonCounts[melon] +=1
+    }  else{
+        melonCounts[melon] = 1
+        } 
+    }
+    return melonCounts
+}
+
+console.log(count_melons(melonsToAdd))
