4742971137f6659c4390ee814117ddaebbabf2e9
Vira Sukhanova <verusia.s@gmail.com>
2021-05-17 21:43:15 -0500

    Added solutions 12-13

--- sql-zoo/queries.sql
+++ sql-zoo/queries.sql
@@ -54,7 +54,25 @@ SELECT name, ROUND(gdp/population, -3)
 FROM world
 WHERE gdp > 1000000000000;
 
--- Show the name and capital where the name and the capital have the same number of characters.
+-- 11. Show the name and capital where the name and the capital have the same number of characters.
 SELECT name, capital
   FROM world
  WHERE LEN(name) = LEN(capital);
+
+
+-- 12. Show the name and the capital where the first letters of each match. 
+-- Don't include countries where the name and the capital are the same word.
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital, 1)
+AND name <> capital;
+
+-- 13. Find the country that has all the vowels and no spaces in its name.
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+    AND name LIKE '%e%'
+    AND name LIKE '%i%'
+    AND name LIKE '%o%'
+    AND name LIKE '%u%'
+    AND name NOT LIKE '% %';
