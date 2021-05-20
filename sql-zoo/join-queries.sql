07e98d1b538185cd7fd6cae52ec57c507c3ab1d4
Vira Sukhanova <verusia.s@gmail.com>
2021-05-20 15:20:34 -0500

    Completed JOIN 1-10

--- sql-zoo/join-queries.sql
+++ sql-zoo/join-queries.sql
@@ -5,22 +5,67 @@ FROM movie
 WHERE yr=1962;
 
 -- 2.Give year of 'Citizen Kane'.
+SELECT yr
+ FROM movie
+ WHERE title='Citizen Kane';
 
 -- 3.List all of the Star Trek movies, include the id, title and yr:
 -- (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr 
+FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr;
 
 -- 4.What id number does the actor 'Glenn Close' have?
+SELECT id
+FROM actor
+WHERE name='Glenn Close';
 
 -- 5.What is the id of the film 'Casablanca'
+SELECT id
+FROM movie
+WHERE title='Casablanca';
 
 -- 6.Obtain the cast list for 'Casablanca': what is a cast list?
 -- Use movieid=11768, (or whatever value you got from the previous question)
+SELECT actor.name
+FROM actor
+JOIN casting
+ON (actor.id = casting.actorid)
+WHERE casting.movieid=11768
 
 -- 7.Obtain the cast list for the film 'Alien'
+SELECT name FROM movie
+JOIN casting ON movie.id = movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE movie.title = 'Alien';
 
 -- 8.List the films in which 'Harrison Ford' has appeared
+SELECT movie.title
+FROM movie
+JOIN casting 
+ON casting.movieid = movie.id
+JOIN actor
+ON actor.id = casting.actorid
+WHERE actor.name='Harrison Ford' 
 
 -- 9.List the films where 'Harrison Ford' has appeared - but not in the starring role. 
 -- [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+SELECT movie.title
+FROM movie
+JOIN casting 
+ON casting.movieid = movie.id
+JOIN actor
+ON actor.id = casting.actorid
+WHERE actor.name='Harrison Ford'
+AND casting.ord !=1
 
 -- 10.List the films together with the leading star for all 1962 films.
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting 
+ON casting.movieid = movie.id
+JOIN actor
+ON actor.id = casting.actorid
+WHERE movie.yr=1962
+AND casting.ord=1
